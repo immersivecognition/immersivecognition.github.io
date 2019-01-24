f7e0c33a1f16aec6a77eedf6a54a51da31d0b396
Jack Brookes <jbrookes187@gmail.com>
2019-01-24 15:57:24 +0000

    fixed urls, frozen

--- source/app/routes.py
+++ source/app/routes.py
@@ -8,7 +8,7 @@ CONTENT = "content"
 def path_to_md(base, filename):
     md_path = os.path.join(CONTENT, base)
     md = markdown2.markdown_path(os.path.join(md_path, filename), extras=["metadata"])
-    md.href = base + '/' + os.path.splitext(filename)[0]
+    md.filename = os.path.splitext(filename)[0]
     return md
 
 
@@ -24,29 +24,28 @@ def index():
     return render_template('index.html')
 
 
-@application.route('/projects')
+@application.route('/projects/')
 def projects():
     return render_template('projects.html', projects=get_mds('projects'))
 
 
-@application.route('/projects/<name>')
+@application.route('/projects/<name>/')
 def project(name):
     project = path_to_md("projects", name + '.md')
     return render_template('project.html', project=project)
 
 
-@application.route('/people')
+@application.route('/people/')
 def people():
     return render_template('people.html', people=get_mds('people'))
 
 
-@application.route('/people/<name>')
+@application.route('/people/<name>/')
 def person(name):
     person = path_to_md("people", name + '.md')
-    print(person.metadata)
     return render_template('person.html', person=person)
 
 
-@application.route('/contact')
+@application.route('/contact/')
 def contact():
     return render_template('contact.html')
---
+++ source/resizeimg.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import os
+import sys
+
+def resize_aspect_fit(path, dest, final_size, fmt):
+    dirs = os.listdir(path)
+    for item in dirs:
+        if item == '.DS_Store':
+            continue
+        if os.path.isfile(path + item):
+            im = Image.open(path + item)
+            f, e = os.path.splitext(path + item)
+            size = im.size
+            ratio = float(final_size) / max(size)
+            new_image_size = tuple([int(x*ratio) for x in size])
+            im = im.resize(new_image_size, Image.ANTIALIAS)
+            new_im = Image.new("RGB" if fmt=='JPEG' else "RGBA", (final_size, final_size))
+            new_im.paste(
+                im, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
+
+            width, height = new_im.size
+
+            left = (width - final_size)/2
+            top = (height - final_size)/2
+            right = (width + final_size)/2
+            bottom = (height + final_size)/2
+
+            new_im.crop((left, top, right, bottom))
+
+            fname, e = os.path.splitext(item)
+            if fmt == 'JPEG':
+                new_im.save(dest + fname + '.jpg', 'JPEG', quality=90)
+            elif fmt == 'PNG':
+                new_im.save(dest + fname + '.png', 'PNG')
+            print(f"converted {item}")
+
+
+resize_aspect_fit("content/people-images/", "app/static/people-images/", 130, 'JPEG')
+resize_aspect_fit("content/project-images/", "app/static/project-images/", 100, 'PNG')
